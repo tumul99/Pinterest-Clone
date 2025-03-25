b53353dcf0a3b1fe228ac2f43457702b2360ee41
Tumul Shukla <tumulshukla912@gmail.com>
2025-03-25 15:35:47 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,27 @@
-const mongoose= require('mongoose');
-const plm=require('passport-local-mongoose');
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/pinterestclone");
+const dbURL = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/pinterestclone";
+
+mongoose.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  fullname: { type: String, required: true, },
+  fullname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String },
-  dp: { type: String, /* assuming the profile picture is stored as url*/ },
+  dp: { type: String },
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }]
 }, { timestamps: true });
 
+userSchema.plugin(plm);
 
+module.exports = mongoose.model('Users', userSchema);
 
 
 userSchema.plugin(plm);
